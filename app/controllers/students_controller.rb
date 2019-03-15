3da62595de1b323a4b010662d3ba04832b1a3029
github username <github email address>
2019-03-15 12:08:59 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,10 @@
 class StudentsController < ApplicationController
   def index
     @students = Student.all
+    render '/students/index'
+  end
+
+  def show
+    @student = Student.find(params[:id])
   end
 end
